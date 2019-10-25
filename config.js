532633dc2becd6dde76c0dca77caac4840d777f9
Joseph McGeever <mcgeevej@coventry.ac.uk>
2019-10-25 17:38:07 +0100

    did a rename on the dB config

--- config.js
+++ config.js
@@ -2,8 +2,8 @@
 exports.config = {
 host: process.env.DB_HOST || "localhost",
 port: process.env.DB_PORT || 3306,
-user: process.env.DB_USER || "root",
+user: process.env.DB_USER || "Joseph",
 password: process.env.DB_PASSWORD || "",
-database: process.env.DB_DATABASE || "oktob",
+database: process.env.DB_DATABASE || "304Project",
 connectionLimit: 100
 };
